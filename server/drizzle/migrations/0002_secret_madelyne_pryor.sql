be6e62f99e5afd54e638eb221cd7eb0973c0e581
MomPansy <jaydenyeo.acs@gmail.com>
2025-04-17 17:42:06 +0800

    latest migrations

--- server/drizzle/migrations/0002_secret_madelyne_pryor.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "messages" ALTER COLUMN "content" DROP NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "auth_user_id" DROP NOT NULL;
-ALTER TABLE "users" ALTER COLUMN "workspace_id" DROP NOT NULL;
-ALTER TABLE "messages" ADD COLUMN "parts" jsonb[];
